870f2d28e7b29ed1f2fc94764f80a22f2ce47b55
JuaniGonza <juanurro27@gmail.com>
2025-09-22 08:43:41 -0300

    Nueva web

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './pages/App.jsx'
+import App from './App.jsx'
 import './assets/base.css'
 
 createRoot(document.getElementById('root')).render(
